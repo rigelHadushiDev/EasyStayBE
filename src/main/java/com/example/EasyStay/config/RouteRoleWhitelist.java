e6f84a6670e3bb5a0aef3f0b221a38deb57b9073
rigelHadushiDev <rigelhadushi4@gmail.com>
2025-05-15 22:44:17 +0200

    minor fix

--- src/main/java/com/example/EasyStay/config/RouteRoleWhitelist.java
+++ src/main/java/com/example/EasyStay/config/RouteRoleWhitelist.java
@@ -10,14 +10,14 @@ public class RouteRoleWhitelist {
 
     static {
         // User routes
-        WHITELIST.put("GET:/user/currentUser", List.of("ADMIN", "RECRUITER", "USER"));
+        WHITELIST.put("GET:/user/currentUser", List.of("ADMIN", "MANAGER", "USER"));
         WHITELIST.put("GET:/user/listUsers", List.of("ADMIN", "RECRUITER"));
-        WHITELIST.put("PATCH:/user/changePassw", List.of("ADMIN", "RECRUITER", "USER"));
-        WHITELIST.put("PATCH:/user", List.of("ADMIN", "RECRUITER", "USER"));
-        WHITELIST.put("DELETE:/user", List.of("ADMIN", "RECRUITER", "USER"));
+        WHITELIST.put("PATCH:/user/changePassw", List.of("ADMIN", "MANAGER", "USER"));
+        WHITELIST.put("PATCH:/user", List.of("ADMIN", "MANAGER", "USER"));
+        WHITELIST.put("DELETE:/user", List.of("ADMIN", "MANAGER", "USER"));
         WHITELIST.put("DELETE:/user/{userId}", List.of("ADMIN"));
-        WHITELIST.put("GET:/user/searchUserFullName", List.of("ADMIN", "RECRUITER"));
+        WHITELIST.put("GET:/user/searchUserFullName", List.of("ADMIN", "MANAGER"));
         WHITELIST.put("POST:/user/create", List.of("ADMIN"));
-        WHITELIST.put("GET:/user/getUser", List.of("ADMIN", "RECRUITER"));
+        WHITELIST.put("GET:/user/getUser", List.of("ADMIN", "MANAGER"));
     }
 }
